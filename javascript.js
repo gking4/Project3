e4b15e0ee8ef8ebbf90fe15804603926f469c7ee
gking4 <gking4@mail.csuchico.edu>
2017-05-10 23:54:17 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -8,5 +8,35 @@ function calcFib()
     fibNumbers.push(fibNumbers[i] + fibnumbers[i+1]);
   }
   var results = fibNumbers[2] + " " + fibNumbers[3] + " " + fibNumbers[4];
+  document.cookie = results;
   document.getElementById("result").innerHTML = results;
+}
+
+function start()
+{
+  var temp = document.cookie;
+    document.getElementById("oldvalues").innerHTML = temp;;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
